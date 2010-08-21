203d014a7809cd6de29b4154596fdc4e63653576
jldupont <github@jldupont.com>
2010-08-21 17:11:32 -0400

    dbus interface work

--- src/app/agents/ratings_dbus.py
+++ src/app/agents/ratings_dbus.py
@@ -14,19 +14,31 @@
     ==========================================================
     
     Dbus Interface:
-    * (IN/OUT) rating(source, timestamp, artist_name, album_name, track_name, rating) 
+    * (IN/OUT) rating(source, ref, timestamp, artist_name, album_name, track_name, rating) 
 
             Upon receiving this signal, the Sync-Playlists will store the rating
             associated with the specified track.
 
             @param source: (string)   the 'source' application generating this rating
-            @param timestamp: (string) the time in seconds since the epoch, in UTC
+            @param ref: (string) (not used on input) opaque reference returned in response to "qrating"
+            @param timestamp: (integer) the time in seconds since the epoch, in UTC
             @param artist_name: (string)
             @param album_name: (string)
             @param track_name: (string)
             @param rating: (integer) [0:100]
             
             Rating signals received with out-dated timestamp are discarded.
+
+    * (IN) qratings(source, ref, timestamp, count)
+    
+            Ratings? from 'timestamp' and descending in time, return a maximum of 'count' records
+            
+            @param source: (string) the 'source' application
+            @param ref: (string) an opaque reference parameter to pass in the response 'rating' signal
+            @param timestamp: (integer) the in seconds since the epoch, in UTC
+            @param count: (integer) the maximum number records to return
+            
+            The response will come in form of "rating" signal(s).
             
     * (IN) qrating(artist_name, album_name, track_name)
     
@@ -68,6 +80,13 @@ class RatingsSignalRx(dbus.service.Object):
                                        dbus_interface="com.systemical.services",
                                        bus_name=None,
                                        path="/ratings"
+                                       )
+                    
+        dbus.Bus().add_signal_receiver(self.rx_qratings,
+                                       signal_name="qratings",
+                                       dbus_interface="com.systemical.services",
+                                       bus_name=None,
+                                       path="/ratings"
                                        )            
         
         dbus.Bus().add_signal_receiver(self.rx_qrating,
@@ -81,8 +100,8 @@ class RatingsSignalRx(dbus.service.Object):
     ## ==========================================================================================
     ## SIGNAL EMITTERS
 
-    @dbus.service.signal(dbus_interface="com.systemical.services", signature="sisssd")
-    def rating(self, source, timestamp, artist_name, album_name, track_name, rating):
+    @dbus.service.signal(dbus_interface="com.systemical.services", signature="ssisssd")
+    def rating(self, source, ref, timestamp, artist_name, album_name, track_name, rating):
         """
         Signal emitter for "/ratings/rating"
         """
@@ -96,11 +115,13 @@ class RatingsSignalRx(dbus.service.Object):
         
     ## ==========================================================================================
     ## SIGNAL RECEIVERS
+    def rx_qratings(self, source, ref, timestamp, count):
+        mswitch.publish(self, "in_qratings", source, ref, timestamp, count)
 
-    def rx_rating(self, source, timestamp, artist_name, album_name, track_name, rating):
-        mswitch.publish(self, "in_rating", source, timestamp, artist_name, album_name, track_name, rating)
+    def rx_rating(self, source, ref, timestamp, artist_name, album_name, track_name, rating):
+        mswitch.publish(self, "in_rating", source, ref, timestamp, artist_name, album_name, track_name, rating)
 
-    def rx_qrating(self, artist_name, album_name, track_name):
+    def rx_qrating(self, source, ref, artist_name, album_name, track_name):
         mswitch.publish(self, "in_qrating", artist_name, album_name, track_name)
 
 
@@ -111,7 +132,7 @@ class DbusAgent(AgentThreadedBase):
         AgentThreadedBase.__init__(self)
         self.srx=RatingsSignalRx(self)
 
-    def h_out_rating(self, source, timestamp, artist_name, album_name, track_name, rating):
+    def h_out_rating(self, source, ref, timestamp, artist_name, album_name, track_name, rating):
         self.srx.rating(source, timestamp, artist_name, album_name, track_name, rating) 
 
     def h_out_updated(self, timestamp):
